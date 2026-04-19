fb1c49222f479cc61ee9dbc4e52513c99612de33
afroze456 <afreenafroze1@gmail.com>
2026-04-19 14:18:59 -0700

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,23 @@
+a=20
+b=32
+c=12
+
+if a>0 and b>0:
+    print("both varibles are greater than 0")
+else:
+    print("one or more numbers are less than 0")
+
+
+a=39
+b=75
+c=-21
+
+if a<0 or b<0:
+    print("atleast on number is greater than 0")
+else:
+    print("none of the numbers are greater then 0")
+
+if b>0 or c>0:
+    print("atleast one number is greater then 0")
+else:
+    print("none of the numbers are greater then 0")
