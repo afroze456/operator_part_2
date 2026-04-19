e9769022ea56d5e3fead0d4fe0d24f5639080184
afroze456 <afreenafroze1@gmail.com>
2026-04-19 14:32:05 -0700

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,24 @@
+a=10
+b=32
+c=19
+
+print (not(a==b))
+
+a="python"
+b="coding"
+
+
+if not (a==b):
+    print("a and b are different.")
+a=4
+b=5
+
+if not((a==1)==(b==5)):
+       print("hello")
+
+a=int(input("enter a number: "))
+
+if not (a%2 == 0):
+    print(a,"is a odd number")
+else:
+     print(a,"is a even number")
